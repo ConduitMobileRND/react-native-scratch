4c8d42f595f41117566587fe4afbd62d12fb03f8
Yitzik Hefetz <yitzik@onoapps.com>
2019-06-23 13:36:28 +0300

    androidX adjustment

--- android/src/main/java/com/reactlibrary/ScratchView.java
+++ android/src/main/java/com/reactlibrary/ScratchView.java
@@ -11,7 +11,7 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import java.util.ArrayList;
 import android.graphics.Rect;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
